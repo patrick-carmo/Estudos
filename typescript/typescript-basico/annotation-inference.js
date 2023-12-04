83b82e5ae2ff2301e1257fbc29c22c5211f0997c
Patrick <ptemy5@gmail.com>
2023-12-04 18:34:05 -0300

    start typescript

---
+++ typescript/typescript-basico/annotation-inference.js
@@ -0,0 +1,23 @@
+"use strict";
+const input = document.querySelector('input');
+const p = document.querySelector('p');
+const total = localStorage.getItem('value');
+if (input && total) {
+    input.value = total;
+    calculate(+input.value);
+}
+function calculate(value) {
+    if (p) {
+        p.innerText = `Ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+function mudance() {
+    if (input) {
+        const value = +input.value;
+        localStorage.setItem('value', value.toString());
+        calculate(value);
+    }
+}
+if (input) {
+    input.addEventListener('input', mudance);
+}
